b6dc30a74e4850a3971f5a2b40e03eac02d46c95
rameshsapkota416@gmail.com <rameshsapkota416@gmail.com>
2022-07-17 18:34:08 +0545

    py

--- RadarPlot.py
+++ RadarPlot.py
@@ -9,7 +9,7 @@ from matplotlib.pyplot import figure
 import plotly.express as px  
 import pandas as pd  
 
-data=pd.DataFrame(dict(keys=[10,20,30,40],values=["Labor cost","Manu Cost","promotion cost","Selling cost"]))
+data=pd.DataFrame(dict(keys=[10,20,30,40,50],values=["Labor cost","Manu Cost","promotion cost","Selling cost","Next Cost"]))
 
 figure=px.line_polar(data,r='keys',theta='values',line_close=True)
 figure.update_traces(fill="toself")
