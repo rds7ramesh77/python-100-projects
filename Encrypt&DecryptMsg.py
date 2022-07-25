823329934111239e4d7d268a852aa3aba07ab15f
rameshsapkota416@gmail.com <rameshsapkota416@gmail.com>
2022-07-25 18:29:15 +0545

    p

---
+++ Encrypt&DecryptMsg.py
@@ -0,0 +1,65 @@
+#python program to encrypt and decrypt message
+
+""" Cryptography means changing the text of a message so that people who don't know your secret never understand your message."""
+
+
+""" To encrypt and decrypt with py, we need to crete a program in which it will first ask you if you want yo encrypt a message or decrypt it
+
+Then program should receive a message from the user."""
+
+
+
+from tkinter import messagebox, simpledialog, Tk
+
+def is_even(number):
+    return number % 2 == 0
+
+def get_even_letters(message):
+    even_letters = []
+    for counter in range(0, len(message)):
+        if is_even(counter):
+            even_letters.append(message[counter])
+    return even_letters
+
+def get_odd_letters(message):
+    odd_letters = []
+    for counter in range(0, len(message)):
+        if not is_even(counter):
+            odd_letters.append(message[counter])
+    return odd_letters
+
+def swap_letters(message):
+    letter_list = []
+    if not is_even(len(message)):
+        message = message + 'x'
+    even_letters = get_even_letters(message)
+    odd_letters = get_odd_letters(message)
+    for counter in range(0, int(len(message)/2)):
+        letter_list.append(odd_letters[counter])
+        letter_list.append(even_letters[counter])
+    new_message = ''.join(letter_list)
+    return new_message
+
+def get_task():
+    task = simpledialog.askstring('Task', 'Do you want to encrypt or decrypt?')
+    return task
+
+def get_message():
+    message = simpledialog.askstring('Message', 'Enter the secret message: ')
+    return message
+
+root = Tk()
+while True:
+    task = get_task()
+    if task == 'encrypt':
+        message = get_message()
+        encrypted = swap_letters(message)
+        messagebox.showinfo('Ciphertext of the secret message is:', encrypted)
+        
+    elif task == 'decrypt':
+        message = get_message()
+        decrypted = swap_letters(message)
+        messagebox.showinfo('Plaintext of the secret message is:', decrypted)
+    else:
+        break
+root.mainloop()
