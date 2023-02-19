f18f686db5e70815a80c5c0331ec8530aac184f1
Ramesh Sapkota <rds7ramesh@gmail.com>
2023-02-19 18:31:52 +0545

    qr

---
+++ qrcodeagain.py
@@ -0,0 +1,16 @@
+import qrcode
+
+def generate_qr_code(text):
+    # Create a QR code instance
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    # Add the data to the QR code
+    qr.add_data(text)
+    # Generate the QR code
+    qr.make(fit=True)
+    # Create an image from the QR code
+    img = qr.make_image(fill_color="black", back_color="white")
+    # Save the image
+    img.save("qr_code.png")
+
+# Generate a QR code for the text "Hello, World!"
+generate_qr_code("Hello, World!")
